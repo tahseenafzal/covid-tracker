6326f232a27e7a19fda182388ef604e3c60220c9
Tehseen Afzal <abuashja@gmail.com>
2021-08-21 23:30:44 +0500

    Update App.js
    Change useState function of country

--- src/App.js
+++ src/App.js
@@ -6,26 +6,21 @@ import coronaImg from "./images/image.png";
 
 function App() {
   const [data, setData] = useState({});
-  const country = useState("");
+  const [country, setCountry] = useState("");
 
-  useEffect(
-    () => {
-      const getData = async () => {
-        country[0]
-          ? setData(await fetchData(country[0]))
-          : setData(await fetchData());
-      };
-      getData();
-    },
-    country[0] ? [country[0]] : []
-  );
+  useEffect(() => {
+    const getData = async () => {
+      country ? setData(await fetchData(country)) : setData(await fetchData());
+    };
+    getData();
+  }, [country]);
 
   return (
     <div className={styles.container}>
       <img className={styles.image} src={coronaImg} alt="COVID-19" />
       <Cards data={data} />
-      <CountryPicker country={country} />
-      <Chart data={data} country={country[0]} />
+      <CountryPicker country={setCountry} />
+      <Chart data={data} country={country} />
     </div>
   );
 }
