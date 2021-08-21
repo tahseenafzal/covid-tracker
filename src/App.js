28ef19de289ed78b87aabd50f1d2e18a31b30528
Tehseen Afzal <abuashja@gmail.com>
2021-08-21 21:50:36 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Cards, Chart, CountryPicker } from "./components/";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+import { fetchData } from "./api";
+import coronaImg from "./images/image.png";
+
+function App() {
+  const [data, setData] = useState({});
+  const country = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      country[0]
+        ? setData(await fetchData(country[0]))
+        : setData(await fetchData());
+    };
+    getData();
+  }, [country[0]]);
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={coronaImg} alt="COVID-19" />
+      <Cards data={data} />
+      <CountryPicker country={country} />
+      <Chart data={data} country={country[0]} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
+import { fetchCountries } from "../../api";
+
+const CountryPicker = ({ country }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      setCountries(await fetchCountries());
+    };
+    getCountries();
+  }, [setCountries]);
+
+  return (
+    <FormControl
+      className={styles.formControl}
+      onChange={(e) => {
+        country[1](e.target.value);
+      }}
+    >
+      <NativeSelect>
+        <option value="">Global</option>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default CountryPicker;
