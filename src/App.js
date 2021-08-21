2db37c72d6c42254d1df93d58b1dd2539b3c877e
Tehseen Afzal <abuashja@gmail.com>
2021-08-21 23:04:47 +0500

    Update App.js
    Update useEffect dependency

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ function App() {
   const [data, setData] = useState({});
   const country = useState("");
 
-  useEffect(() => {
-    const getData = async () => {
-      country[0]
-        ? setData(await fetchData(country[0]))
-        : setData(await fetchData());
-    };
-    getData();
-  }, [country[0] ? country[0] : null]);
+  useEffect(
+    () => {
+      const getData = async () => {
+        country[0]
+          ? setData(await fetchData(country[0]))
+          : setData(await fetchData());
+      };
+      getData();
+    },
+    country[0] ? [country[0]] : []
+  );
 
   return (
     <div className={styles.container}>
